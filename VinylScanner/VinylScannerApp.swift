2da5d62cb4527c9d739484494a884f01379a5e91
Serena <osama.id78@gmail.com>
2023-12-01 11:04:00 +0300

    Initial Commit

---
+++ VinylScanner/VinylScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylScannerApp.swift
+//  VinylScanner
+//
+//  Created by Serena on 01/12/2023.
+//  
+
+import SwiftUI
+
+@main
+struct VinylScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
